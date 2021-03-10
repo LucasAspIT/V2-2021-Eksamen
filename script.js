c6b3cadb793480a73ddd01bffa33586b15a846a4
LucasAspIT <luca609a@edu.campusvejle.dk>
2021-03-10 13:45:49 +0100

    Fixed navigation menu bug on screen size change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // NAVIGATION MENU ##############################################
 const navToggle = document.querySelector("#toggle");
 const navPosition = document.querySelector(".nav");
+const mediaQuery = window.matchMedia("(min-width: 1200px)");
 
 // Make sure it's unchecked by default
 navToggle.checked = false;
@@ -10,6 +11,18 @@ navToggle.addEventListener("change", function () {
   if (this.checked) {
     navPosition.style.right = "0";
   } else {
-    navPosition.style.right = `-${navPosition.offsetWidth}px`;
+    navPosition.style.right = `-100%`;
+  }
+});
+
+// Make sure the navigation properly transitions and resets on design change
+mediaQuery.addEventListener("change", function () {
+  if (this.matches) {
+    navPosition.style.right = "0";
+    // navPosition.style.transition = "right 0.0s";
+  } else {
+    navToggle.checked = false;
+    navPosition.style.right = `-100%`;
+    // navPosition.style.transition = "right 0.5s";
   }
 });
